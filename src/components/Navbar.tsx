2057545066ad6f424d7ee4933f9611e092eb3b1e
AmanGoel-Dev <goelaman79060@gmail.com>
2026-01-12 13:33:47 +0530

    added sign meesage featuer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,13 @@ import { useBalance } from "@/context/Balancecontext";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const { connected } = useWallet();
   const { balance, fetchBalance } = useBalance();
   const wallet = useWallet();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (wallet.publicKey && wallet.connected) {
@@ -18,7 +20,7 @@ export default function Navbar() {
   return (
     <nav className="w-full bg-black border-b border-gray-800 px-6 py-4">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
-        <h1 className="text-xl font-bold bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent">
+        <h1 className="text-xl font-bold bg-linear-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent">
           Solana dApp
         </h1>
 
@@ -34,14 +36,18 @@ export default function Navbar() {
           </div>
 
           <button
-            className="px-4 py-2 rounded-lg text-sm font-medium 
+            className="px-4 py-2 rounded-lg text-sm font-medium cursor-pointer
             bg-gray-900 text-gray-200 hover:bg-gray-800 transition"
+            onClick={() => navigate("/send")}
           >
             Send
           </button>
 
           <button
-            className="px-4 py-2 rounded-lg text-sm font-medium 
+            onClick={() => {
+              navigate("/sign");
+            }}
+            className="px-4 py-2 rounded-lg text-sm font-medium cursor-pointer
             bg-gray-900 text-gray-200 hover:bg-gray-800 transition"
           >
             Sign Message
--- src/components/Sign.tsx
+++ src/components/Sign.tsx
@@ -1,7 +1,94 @@
-import React from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { ed25519 } from "@noble/curves/ed25519.js";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import toast from "react-hot-toast";
 
 const Sign = () => {
-  return <div>Sign</div>;
+  const wallet = useWallet();
+  const onclick = async () => {
+    if (!wallet.publicKey) {
+      toast.error("Login to wallet");
+      return;
+    }
+    // sgin message is a optional funcitonality so we need to check if it exists or not in this wallet
+    if (wallet.signMessage === undefined) {
+      toast.error("Wallet does not support message signing");
+      return;
+    }
+    const message = (document.getElementById("message") as HTMLInputElement)
+      .value;
+
+    if (!message) {
+      toast.error("Enter the message to sign");
+    }
+    const encodemsg = new TextEncoder().encode(message);
+    const signedmessage = await wallet.signMessage(encodemsg);
+    const verification = ed25519.verify(
+      signedmessage,
+      encodemsg,
+      wallet.publicKey.toBytes()
+    );
+    if (verification) {
+      toast.success("Message signed and verified successfully");
+    } else {
+      toast.error("Message verification failed");
+    }
+    (document.getElementById("message") as HTMLInputElement).value = "";
+  };
+  return (
+    <div className=" flex items-center justify-center min-h-[calc(100vh-64px)]">
+      <Card
+        className="w-full max-w-sm 
+    bg-black/40
+    backdrop-blur-xl
+    border border-white/30
+    shadow-[0_8px_32px_rgba(0,0,0,0.5)]
+    rounded-2xl
+    text-gray-200"
+      >
+        <CardHeader>
+          <CardTitle> Verify using Sign </CardTitle>
+          <CardDescription>Sign the message</CardDescription>
+          <CardAction>
+            <Button
+              className=" text-black cursor-pointer"
+              onClick={onclick}
+              variant="outline"
+            >
+              Sign Message
+            </Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="message">Message</Label>
+                <Input
+                  id="message"
+                  type="text"
+                  placeholder="Enter the Message"
+                  required
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2"></CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default Sign;
